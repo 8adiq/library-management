302cbadaf933302cb67a6d9aa4c6d74374380bf4
8adiq <abdul.latif708@yahoo.com>
2024-08-19 11:02:32 +0000

    inout validation and error handling done

--- library.py
+++ library.py
@@ -1,6 +1,5 @@
 import os
 import psycopg2
-from psycopg2 import OperationalError
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,63 +21,95 @@ class Library:
             )
             print(" Successfully Connected")
             return self.connection
-        except OperationalError as e:
+        except psycopg2.OperationalError as e:
             print(f" The error {e} occured")
             return None
         
     def add_book(self,title,author,year):
         """ function to add a new book"""
-        cur = self.connection.cursor()
 
-        # checking if book already exists
-        cur.execute("SELECT * from books WHERE title = %s AND author = %s AND year_pub =%s", (title,author,year))
-        existing_book = cur.fetchone()
+        if not year.isdigit() or len(year)<4:
+            print('Enter a valid 4-digit year.')
+            return
+        
+        if not title.strip() or author.strip():
+            print(' Title and author can not be empty')
+            return
+        
+        try :
+            cur = self.connection.cursor()
+            # checking if book already exists
+            cur.execute("SELECT * from books WHERE title = %s AND author = %s AND year_pub =%s", (title,author,year))
+            existing_book = cur.fetchone()
 
-        if existing_book:
-            print(f'{title} by {author} already in the Library')
-        else:
-            cur.execute("INSERT INTO books (title,author,year_pub) VALUES (%s,%s,%s)", (title,author,year))
-            self.connection.commit()
-            print(f'{title} by {author} has been added.')
-        cur.close()
+            if existing_book:
+                print(f'{title} by {author} already in the Library')
+            else:
+                cur.execute("INSERT INTO books (title,author,year_pub) VALUES (%s,%s,%s)", (title,author,year))
+                self.connection.commit()
+                print(f'{title} by {author} has been added.')
+            cur.close()
+        except psycopg2.Error as e:
+            print(f' The error {e} occured.')
         
     def search_book(self,search_item):
         """function to search for a book"""
-        title,author = search_item.split(', ')
-        cur = self.connection.cursor()
+        try:
+            title,author = search_item.split(', ')
+        except ValueError:
+            print('Please enter your search item in the format "Title, Author"')
+            return
 
-        cur.execute("SELECT * from books WHERE title = %s AND author = %s", (title,author))
-        existing_book = cur.fetchone()
-
-        if existing_book:
+        try:
+            cur = self.connection.cursor()
             cur.execute("SELECT title, author,year_pub from books WHERE title = %s AND author = %s ", (title,author))
-            results = cur.fetchall()
+            existing_book = cur.fetchone()
 
-            for title,author,year in results:
+            if existing_book:
+                title,author,year = existing_book
                 print(f"{title} by {author} published in {year}")
-        else:
-            print(f'No book with title {title} by {author}')
+            else:
+                print(f'No book with title {title} by {author}')
+        except psycopg2.Error as e:
+            print(f' The error {e} occured.')
 
     def display_books(self):
         """function to display all books in the library"""
-        cur = self.connection.cursor()
-        cur.execute('SELECT title, author,year_pub FROM books')
-        books = cur.fetchall()
-        print('\n ====== All books in the library ======')
-        for i, (title,author,year) in enumerate(books,start=1):
-            print(f'{i}.  {title} by {author} published in {year}')
+
+        try:
+            cur = self.connection.cursor()
+            cur.execute('SELECT title, author,year_pub FROM books')
+            books = cur.fetchall()
+            print('\n ====== All books in the library ======')
+            for i, (title,author,year) in enumerate(books,start=1):
+                print(f'{i}.  {title} by {author} published in {year}')
+        except psycopg2.Error as e:
+            print(f' The error {e} occured.')
 
     def remove_book(self,title,author):
         """function to remove a book from the library"""
-        cur = self.connection.cursor()
-        cur.execute("DELETE FROM books WHERE title = %s AND author = %s", (title,author))
+        if not title.strip() or author.strip():
+            print(' Title and author can not be empty')
+            return
+        
+        try:
+            cur = self.connection.cursor()
+            cur.execute("DELETE FROM books WHERE title = %s AND author = %s", (title,author))
 
-        if cur.rowcount == 0:
-            print(f" No book with the title {title} by {author}")
-        else:
-            self.connection.commit()
-            print(f" {title} by {author} has been removed")
-        cur.close()
+            if cur.rowcount == 0:
+                print(f" No book with the title {title} by {author}")
+            else:
+                self.connection.commit()
+                print(f" {title} by {author} has been removed")
+            cur.close()
+        except psycopg2.Error as e:
+            print(f' The error {e} occured.')
+
+    def close_connection(self):
+        """closing connection to database"""
+        if self.connection:
+            self.connection.close()
+            print('Database connection closed')
 
 
 def main():
@@ -107,6 +138,7 @@ def main():
             library.remove_book(title,author)
         elif choice == '5':
             print("Exiting the system. Goodbye!")
+            library.close_connection()
             break
         else:
             print("Invalid choice. Please enter a number between 1 and 5.")
